76354f4436ecb43dd3d147080865dc463db91e3c
kdicari24@gmail.com <kdicari24@gmail.com>
2022-09-13 14:05:26 -0400

    added model structure and began relationship identities

---
+++ models/Items.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Item extends Model { }
+
+Item.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        item_name: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        strength: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'item',
+    }
+);
+
+module.exports = Item;
