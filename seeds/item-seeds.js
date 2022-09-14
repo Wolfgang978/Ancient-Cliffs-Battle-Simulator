8e737b6fcedca85a2a94209a5e7e2fa53e73154f
kdicari24@gmail.com <kdicari24@gmail.com>
2022-09-14 13:00:35 -0400

    saving changes for push

--- seeds/item-seeds.js
+++ seeds/item-seeds.js
@@ -4,22 +4,26 @@ const itemData = [
   {
     item_name: 'Katana',
     description: 'Quick and agile melee item',
-    strength: 15, 
+    strength: 15,
+    characters_id: 1 
   },
   {
     item_name: 'long sword',
     description: 'versatile melee item',
     strength: 25, 
+    characters_id: 2
   },
   {
     item_name: 'staff',
     description: 'helps mage conjure magical abilities',
     strength: 35, 
+    characters_id: 3 
   },
   {
     item_name: 'long bow',
     description: 'Bow and arrow for accuracy at range',
-    strength: 30, 
+    strength: 30,
+    characters_id: 4 
   }
 ]
 
