ed676e06b4e70316167e9cd428961de8351c041e
Wolfgang <18wrightm@gmail.com>
2022-09-14 13:48:40 -0400

    character creation done

--- controllers/api/character-creation.js
+++ controllers/api/character-creation.js
@@ -28,7 +28,22 @@ router.get('/:id', async (req, res) => {
     }
   }
 );
-
+router.post('/', async (req, res) => {
+  try {
+    const characterData = await Characters.create(req.body);
+    res.status(200).json(characterData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+router.post('/item', async (req, res) => {
+  try {
+    const itemData = await Item.create(req.body);
+    res.status(200).json(itemData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
 
