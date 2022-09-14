2005e79b39913dad5494e462341b7b93625b0da8
Wolfgang <18wrightm@gmail.com>
2022-09-14 13:31:34 -0400

    routes done?

--- controllers/api/battle.js
+++ controllers/api/battle.js
@@ -4,50 +4,13 @@ const { Character, Item } = require('../../models');
 router.get('/:id', async (req, res) => {
   
   try {
-    const characterData = await Character.findByPk(req.params.id, {
+    const characterData = await Characters.findByPk(req.params.id, {
       include: [
-        {
-          model: Character,
-          attributes: [
-            'id',
-            'character_name',
-            'description',
-            'hitpoints',
-            
-          ],
-          
-        },
+        { model: Characters, model: Item, },
       ],
     });
-    const character = characterData.get({ plain: true });
-    return character
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-}
-);
-
-router.get('/:id', async (req, res) => {
-  
-  try {
-    const itemData = await Item.findByPk(req.params.id, {
-      include: [
-        {
-          model: Item,
-          attributes: [
-            'id',
-            'item_name',
-            'description',
-            'strength',
-            
-          ],
-          
-        },
-      ],
-    });
-    const item = itemData.get({ plain: true });
-    return item
+    res.status(200).json(characterData);
+    
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
