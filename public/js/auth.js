263cb144b4330f78da1c657b91f28c450cd244f1
kdicari24@gmail.com <kdicari24@gmail.com>
2022-09-18 12:39:25 -0400

    saving changes for pull

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,13 +1,11 @@
 const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
+
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
   const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    // TODO: Add a comment describing the functionality of this expression
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
@@ -23,15 +21,15 @@ const loginFormHandler = async (event) => {
 };
 
 const signupFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
+
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
+
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
   if (username && password) {
-    // TODO: Add a comment describing the functionality of this expression
+
     const response = await fetch('/api/users/signup', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
